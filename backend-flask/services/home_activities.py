7a25a2f24a3ed0fa735ce440de3017b962fcceb8
Nnamdi89 <ezugwuaugustine@gmail.com>
2023-03-06 02:25:03 +0000

    modified code to set UserId as string

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -7,9 +7,9 @@ class HomeActivities:
     # logger.info(HomeActivities)
     with tracer.start_as_current_span("home-activities-mock-data"):
       span = trace.get_current_span()
-      username = ['Andrew Brown', 'Garek', 'Worf', 'Cocomelon']
-      senders_username = random.choices(username)
-      now = datetime.now(timezone.utc).astimezone()
+      username = ['Andrew Brown', 'Garek', 'Worf', 'Cocomelon'] # custom code to add UserId as honeycomb attribute
+      senders_username = random.choice(username)
+      now = datetime.now(timezone.utc).astimezone() 
       span.set_attribute("app.now", now.isoformat())
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
