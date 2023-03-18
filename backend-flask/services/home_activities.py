c12fbb968178e9096d9e8eea53508f4ccf69a7ab
Nnamdi89 <ezugwuaugustine@gmail.com>
2023-03-18 19:31:08 +0000

    we have a query working

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -6,14 +6,27 @@ tracer = trace.get_tracer("home.activities")
 class HomeActivities:
   def run(cognito_user_id = None):
     # logger.info(HomeActivities)
-    with tracer.start_as_current_span("home-activities-mock-data"):
-      span = trace.get_current_span()
+    #with tracer.start_as_current_span("home-activities-mock-data"):
+     # span = trace.get_current_span()
       #username = ['Andrew Brown', 'Garek', 'Worf', 'Cocomelon'] # custom code to add UserId as honeycomb attribute
       #senders_username = random.choice(username)
-      now = datetime.now(timezone.utc).astimezone() 
-      span.set_attribute("app.now", now.isoformat())
+     # now = datetime.now(timezone.utc).astimezone() 
+     # span.set_attribute("app.now", now.isoformat())
       sql = query_wrap_array("""
-      SELECT * FROM activities
+      SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
       """)
       with pool.connection() as conn:
         with conn.cursor() as cur:
